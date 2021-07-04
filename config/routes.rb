653019cb80a46dd1071bc16b5cf0f9f056e88af0
chiaki.ueno <s1c2r6@gmail.com>
2021-07-04 23:50:24 +0900

    add: UI勤怠一覧表示画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   
+  get 'show', to: 'users#show'
+
+
+  resources :users, only: [:show]
 end
