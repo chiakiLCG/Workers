92c88583d83d6aefddbcb8191b2797041774cb1d
shusakuueno <shu.ueno@gmail.com>
2021-12-10 11:40:46 +0900

    業務時間一覧画面へのアクセスをusersディレクトリ→worksディレクトリに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  
-  get 'show', to: 'users#show'
+
+  get 'index', to: 'works#index'
   get 'paid_holidays', to: 'works#paid_holidays'
 
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, only: [:show]
+  resources :works, only: [:index]
 end
