96d0f44e26aca0355d7a456348fa2795dc62968c
shusakuueno <shu.ueno@gmail.com>
2021-12-13 14:31:07 +0900

    time処理をviewではなくcontrollerに記述

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,8 @@
 class WorksController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
-  def index; end
+  def index
+    @time = Time.now.strftime('%Y/%m/%d %H:%M')
+  end
 
   def show; end
 
